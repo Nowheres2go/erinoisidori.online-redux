cb9f1a5198249b279c18bc1152c391e179c11eba
webcrawls <github.com@kaden.sh>
2024-06-06 13:36:01 -0400

    💄 intermediate commit
    * i don't remember what i was doing last night

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,9 @@ module.exports = function(config) {
     config.addPassthroughCopy("./assets/styles/**")
     config.addPassthroughCopy("./assets/svg/**")
     config.addPassthroughCopy("./assets/scripts/**")
-}
+    config.addPassthroughCopy("./assets/blog/**")
+
+
+    config.addFilter("shortDate", dateObj => {
+        return dateObj.toLocaleDateString('en-CA');
+    });}
