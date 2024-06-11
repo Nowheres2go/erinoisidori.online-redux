fa94ade1bc552b3ad80738495c427251cbbd5a84
webcrawls <github.com@kaden.sh>
2024-06-11 18:59:17 -0400

    ⭐ add 'postType' filter to 11ty, display accurate post type in cards layout

--- .eleventy.js
+++ .eleventy.js
@@ -11,6 +11,23 @@ module.exports = function (config) {
         return JSON.stringify(data)
     });
 
+
+    // determines and returns the "pretty name" of the provided post's type. i.e. posts in the 'art' collection are 'artwork's.
+    config.addFilter("postType", data => {
+        const names = {
+            'art': 'artwork',
+            'posts': 'blog post',
+        }
+
+        for (const tag of data.data.tags) {
+
+            console.log(tag, data.data.tags)
+            if (!!names[tag]) return names[tag]
+        }
+
+        return ""
+    })
+
     config.addCollection('works', function (collection) {
         return collection
             .getAll()
